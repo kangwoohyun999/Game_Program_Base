d098b17cf4f4b69abf54637c0b2ad9f1bb78dc6d
wh1012 <63694003+kangwoohyun999@users.noreply.github.com>
2026-06-21 13:57:28 +0900

    update

--- final_game/Dead_City/Assets/Scripts/IDamageable.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-// 데미지를 입을 수 있는 타입 인터페이스 (15주차 - 느슨한 커플링)
-// Zombie2_1_R 원본 그대로 유지
-public interface IDamageable
-{
-    void OnDamage(float damage, Vector3 hitPoint, Vector3 hitNormal);
-}
--- final_game/Dead_City/Assets/Scripts/IItem.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public interface IItem
-{
-    void Use(GameObject target);
-}
